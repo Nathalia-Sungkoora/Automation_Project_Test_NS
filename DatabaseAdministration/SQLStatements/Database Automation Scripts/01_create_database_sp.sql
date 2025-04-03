f8270c3424ed3b1f3be284489a91850ca9e6d11c
Nathalia_Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-04-03 18:04:49 +0200

    Create 01_create_database_sp.sql

---
+++ DatabaseAdministration/SQLStatements/Database Automation Scripts/01_create_database_sp.sql
@@ -0,0 +1,21 @@
+--Creating a database
+-- 1. Set context to master database (system database)
+USE master;
+GO
+
+-- 2. Create or modify a stored procedure named CreateAutoDatabase
+CREATE OR ALTER PROCEDURE CreateAutoDatabase
+AS
+BEGIN
+	-- 3. Check if database 'AutoDBNathalia' exists
+    IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'AutoTest_NS_03April')
+    BEGIN
+	-- 4. If it doesn't exist, create it
+        EXEC('CREATE DATABASE AutoTest_NS_03April');
+    END
+END;
+GO
+
+-- 5. Run the stored procedure
+EXEC CreateAutoDatabase;
+GO
